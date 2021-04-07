bff90a61205c9ce31cb5b86b4e6a4ba3b5b776e2
mariovoorbij <m.voorbij@carepay.com>
2021-04-07 22:26:17 +0200

    Added the tradingpair entity and service

---
+++ services/TradingPairService.py
@@ -0,0 +1,30 @@
+import json
+import entity.tradingPair
+
+tradingPairList = []
+
+def FetchTradingPairs():
+    # Fetch a list of trading pairs for which quotes should be fetched
+    with open('tradingpairs.json') as json_file:
+        JSONFromFile = json.load(json_file)
+        tradingPairList = ConvertToList(JSONFromFile)
+        return tradingPairList
+
+def ConvertToList(tradingPairJSON):
+    for tradingPairFromJSON in tradingPairJSON:
+        # convert each trading pair to a trading pair entity object
+        tradingPairToAdd = entity.tradingPair.TradingPair(tradingPairFromJSON["baseToken"],
+                                                    tradingPairFromJSON["swapToken"],
+                                                    tradingPairFromJSON["moonBagPercentage"],
+                                                    tradingPairFromJSON["allocationPercentage"],
+                                                    tradingPairFromJSON["takeProfitPercentage"],
+                                                    tradingPairFromJSON["minimumOrderSize"],
+                                                    tradingPairFromJSON["pathPreferred"],
+                                                    tradingPairFromJSON['maxOutstandingBuyOrders'],
+                                                    tradingPairFromJSON["slippage"],
+                                                    tradingPairFromJSON["baseTokenAddress"],
+                                                    tradingPairFromJSON["swapTokenAddress"],
+                                                    tradingPairFromJSON["dateTimeStamp"])
+        # now add the entity object to the list
+        tradingPairList.append(tradingPairToAdd)
+    return tradingPairList
